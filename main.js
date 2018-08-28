3fe19783d2da39af9ddfeb576f210b938356f8a9
Aleksi Liukkonen <aleksiliukkonen@Aleksis-MacBook-Pro.local>
2018-08-28 11:50:33 +0300

    Refactor front page trending movie

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const form = document.querySelector('form');
 const movies_div = document.querySelector('.movies_container');
 const input = document.querySelector('input');
 const loader = document.querySelector('.loader');
-const trending_div = document.querySelector('.trending_movie');
-const trending_info = document.querySelector('.trending_movie_info');
+const trending = document.querySelector('.trending');
 
 const state = {
   searchTerm: '',
@@ -33,11 +32,6 @@ function renderTrending() {
   h2.innerHTML = state.trending.original_title;
   h3.innerHTML = state.trending.release_date;
   p.innerHTML = `${state.trending.vote_average} <span class="rating_text">Rating</span>`;
-  const a = document.createElement('a');
-  const linkText = document.createTextNode('More Details ›');
-  a.appendChild(linkText);
-  a.title = 'More Details';
-  a.href = 'movie.html' + '?search=' + '&' + 'movieId=' + state.trending.id;
   const img = document.createElement('img');
   img.src = `https://image.tmdb.org/t/p/w500${state.trending.poster_path}`;
   img.classList.add('movies_img'); // rgba(64, 64, 122, .5),
@@ -46,11 +40,23 @@ function renderTrending() {
     rgba(0, 0, 0, .1),  
     rgba(44, 44, 84, .7)
   ), url('https://image.tmdb.org/t/p/w1280${state.trending.backdrop_path}')`;
-  trending_info.appendChild(h2);
-  trending_info.appendChild(h3);
-  trending_div.appendChild(p);
-  trending_info.appendChild(a);
-  trending_div.appendChild(img);
+  const a = document.createElement('a');
+  a.href =  'movie.html' + '?search=' + '&' + 'movieId=' + state.trending.id;
+  a.classList.add('trending_movie');
+  const trending_movie_info = document.createElement('div');
+  const trending_movie_rating = document.createElement('div');
+  const trending_movie_pic = document.createElement('div');
+  trending_movie_info.classList.add('trending_movie_info');
+  trending_movie_rating.classList.add('trending_movie_rating');
+  trending_movie_pic.classList.add('trending_movie_pic');
+  a.appendChild(trending_movie_info);
+  a.appendChild(trending_movie_rating);
+  a.appendChild(trending_movie_pic);
+  trending_movie_info.appendChild(h2);
+  trending_movie_info.appendChild(h3);
+  trending_movie_rating.appendChild(p);
+  trending_movie_pic.appendChild(img);
+  trending.appendChild(a);
 }
 
 getTrendingData()
